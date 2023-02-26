20761b27ba357a5b07fccf58386290559e10a656
Thach Nguyen <thachnn80@gmail.com>
2023-02-26 12:54:15 +0700

    Import from official released v8.19.4

--- docs/bin/build.js
+++
@@ -1,9 +0,0 @@
-const run = require('../lib/build.js')
-const { paths } = require('../lib/index')
-
-run(paths)
-  .then((res) => console.log(`Wrote ${res.length} files`))
-  .catch((err) => {
-    process.exitCode = 1
-    console.error(err)
-  })
